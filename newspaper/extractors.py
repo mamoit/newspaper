45a7264485b43e698bc2cb36e21f358f2a6b7f64
Miguel Almeida <migaxmoitax@gmail.com>
2017-07-21 11:51:31 +0100

    Add og:image:secure_url and og:image:url as image scraping sources

--- newspaper/extractors.py
+++ newspaper/extractors.py
@@ -458,6 +458,11 @@ class ContentExtractor(object):
         if top_meta_image is None:
             top_meta_image = self.get_meta_content(doc, 'meta[name="og:image"]')
 
+        if top_meta_image is None:
+            top_meta_image = self.get_meta_content(doc, 'meta[name="og:image:secure_url"]')
+
+        if top_meta_image is None:
+            top_meta_image = self.get_meta_content(doc, 'meta[name="og:image:url"]')
 
         if top_meta_image:
             return urljoin(article_url, top_meta_image)
